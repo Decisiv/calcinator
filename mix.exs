0d161e8d89c98514e146ae79afeae65215b94388
Edipo Vinicius da Silva <edipox@gmail.com>
2018-10-30 18:45:37 -0300

    [MISC] Add missing data required to publish (#4)

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule Calcinator.Mixfile do
       app: :calcinator,
       build_embedded: Mix.env() == :prod,
       deps: deps(),
+      organization: "decisiv",
       description: description(),
       docs: docs(),
       elixir: "~> 1.3",
@@ -123,6 +124,8 @@ defmodule Calcinator.Mixfile do
 
   defp docs do
     [
+      main: "readme",
+      source_url: repo_url(),
       extras: ~w(CHANGELOG.md CODE_OF_CONDUCT.md CONTRIBUTING.md LICENSE.md README.md UPGRADING.md)
     ]
   end
@@ -145,11 +148,21 @@ defmodule Calcinator.Mixfile do
     [
       files: ["lib", "mix.exs" | extras()],
       licenses: ["Apache 2.0"],
+      maintainers: [
+        "Edipo da Silva",
+        "Julian Skinner",
+        "Luke Imhoff",
+        "Mayra Céspedes",
+        "Ryan Wojno"
+      ],
       links: %{
-        "Docs" => "https://hexdocs.pm/calcinator",
-        "Github" => "https://github.com/Decisiv/calcinator"
+        "Github" => repo_url()
       },
-      maintainers: ["Luke Imhoff"]
+      organization: "decisiv"
     ]
+  end
+
+  defp repo_url do
+    "https://github.com/Decisiv/calcinator"
   end
 end
